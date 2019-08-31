410e20d1b08961206f8c668d19e48e0fe7d353c9
artem <artemsh@mmvs.ru>
2019-08-31 14:17:04 +0500

    Add files kind of application

--- app/src/main/java/ru/artemsh/touristRoutes/route/RouteActivity.java
+++ app/src/main/java/ru/artemsh/touristRoutes/route/RouteActivity.java
@@ -20,15 +20,6 @@ public class RouteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_route);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
