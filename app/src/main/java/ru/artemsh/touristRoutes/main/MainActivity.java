7460a51fcdff359f90e80b9a0bd524064e5030dd
artem <artemsh@mmvs.ru>
2019-09-12 00:51:27 +0500

    Made map

--- app/src/main/java/ru/artemsh/touristRoutes/main/MainActivity.java
+++ app/src/main/java/ru/artemsh/touristRoutes/main/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.artemsh.touristRoutes.main;
 
 
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -12,6 +13,8 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import ru.artemsh.touristRoutes.R;
@@ -34,12 +37,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (android.os.Build.VERSION.SDK_INT >= 19) {
+            int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            getWindow().getDecorView().setSystemUiVisibility(flags);
+        }
+
         showplace = new ShowplaceFragment();
         wasPlace = new WasPlaceFramgent();
         map = new MapFragment();
 
         navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navView.setSelectedItemId(R.id.navigation_map);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
